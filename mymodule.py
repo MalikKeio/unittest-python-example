3bd5b31b67ae149b98d67dd27b5869421db12382
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-07-18 14:48:16 +0900

    Tests are successful and the program works

--- mymodule.py
+++ mymodule.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
+def removeTags(string):
+    insideTag = False
+    noHTMLStr = ""
+    for c in string:
+        if c == "<":
+            insideTag = True
+        if not insideTag:
+            noHTMLStr += c
+        if c == ">":
+            insideTag = False
+    return noHTMLStr
+
+def stripSpaces(string):
+    strippedStr = ""
+    string = string.strip()
+    onSpaces = False
+    spaceInserted = False
+    for c in string:
+        beforeOnSpaces = onSpaces
+        onSpaces = c == " " or c == "\n"
+        if not beforeOnSpaces and onSpaces:
+            spaceInserted = False
+        if not onSpaces:
+            strippedStr += c
+        elif onSpaces and not spaceInserted:
+            strippedStr += " "
+            spaceInserted = True
+    return strippedStr
+
 def myfunction(string):
-	return string.strip()
+    noHTMLStr = removeTags(string)
+    strippedStr = stripSpaces(noHTMLStr)
+    return strippedStr
--- unit/test.py
+++ unit/test.py
@@ -1,16 +1,103 @@
 import unittest
 import mymodule
 
+class TestRemoveTags(unittest.TestCase):
+    def test_empty_input(self):
+        inputString = ""
+        self.assertEqual(mymodule.removeTags(inputString), "")
+
+    def test_1(self):
+        inputString = "<h2>     fetr fertgg     gh</h2>"
+        self.assertEqual(mymodule.removeTags(inputString), "     fetr fertgg     gh")
+
+    def test_one_tag(self):
+        inputString = "<a></a>"
+        output = mymodule.removeTags(inputString)
+        self.assertEqual(output, "")
+
+    def test_one_tag_with_spaces(self):
+        inputString = "<a>    </a>"
+        output = mymodule.removeTags(inputString)
+        self.assertEqual(output, "    ")
+
+    def test_one_tag_with_spaces_and_newlines(self):
+        inputString = """<a>
+
+</a>"""
+        output = mymodule.removeTags(inputString)
+        self.assertEqual(output, """
+
+""")
+
+    def test_lots_of_tags_with_spaces_and_newlines(self):
+        inputString = """<a> TITLE
+            POIRER
+        </a><span>treter</span>"""
+        output = mymodule.removeTags(inputString)
+        expectedOutput = """ TITLE
+            POIRER
+        treter"""
+        self.assertEqual(output, expectedOutput)
+
+    def test_lots_of_tags_with_spaces_and_newlines_with_unicode(self):
+        inputString = """<a> 日本語
+            POIRER ひらがな <p>See for yourself</p>
+        </a>"""
+        output = mymodule.removeTags(inputString)
+        expectedOutput = """ 日本語
+            POIRER ひらがな See for yourself
+        """
+        self.assertEqual(output, expectedOutput)
+
+class TestStringSpaces(unittest.TestCase):
+    def test_empty_input(self):
+        inputString = ""
+        self.assertEqual(mymodule.stripSpaces(inputString), "")
+    def test_1(self):
+        inputString = "     fetr fertgg     gh"
+        self.assertEqual(mymodule.stripSpaces(inputString), "fetr fertgg gh")
+
+
+
 class TestMymodule(unittest.TestCase):
 
     def test_empty_input(self):
         inputString = ""
         self.assertEqual(mymodule.myfunction(inputString), "")
 
-    def test_stupid_test_that_will_fail(self):
-        inputString = ["Arrayを入力して意味あんの？", 3]
-        self.assertEqual(mymodule.myfunction(inputString), "")
-    def test_stupid_test_error(self):
-        """あるはテストする価値のある不可欠な機能ですか？"""
-        inputString = ["Arrayを入力して意味あんの？", 3]
-        self.assertRaises(AttributeError, mymodule.myfunction, inputString)
+    def test_1(self):
+        inputString = "<h2>     fetr fertgg     gh</h2>"
+        self.assertEqual(mymodule.myfunction(inputString), "fetr fertgg gh")
+
+    def test_one_tag(self):
+        inputString = "<a></a>"
+        output = mymodule.myfunction(inputString)
+        self.assertEqual(output, "")
+
+    def test_one_tag_with_spaces(self):
+        inputString = "<a>    </a>"
+        output = mymodule.myfunction(inputString)
+        self.assertEqual(output, "")
+
+    def test_one_tag_with_spaces_and_newlines(self):
+        inputString = """<a>
+
+         </a>"""
+        output = mymodule.myfunction(inputString)
+        self.assertEqual(output, "")
+
+    def test_lots_of_tags_with_spaces_and_newlines(self):
+        inputString = """<a> TITLE
+            POIRER
+        </a><span>treter</span>"""
+        output = mymodule.myfunction(inputString)
+        expectedOutput = "TITLE POIRER treter"
+        self.assertEqual(output, expectedOutput)
+
+    def test_lots_of_tags_with_spaces_and_newlines_with_unicode(self):
+        inputString = """<a> 日本語
+            POIRER ひらがな <p>See for yourself  </p>
+        </a>"""
+        output = mymodule.myfunction(inputString)
+        expectedOutput = "日本語 POIRER ひらがな See for yourself"
+        self.assertEqual(output, expectedOutput)
